52e01f89ec2e97c3f45b807c9bc54fa9f1b01cc9
Bhaumik <bhaumik182001@gmail.com>
2026-03-10 17:31:59 +0530

    fixed path version issue by specifying the version for flyway and mapper

--- src/main/java/com/bhaumik18/finguard/config/DatabaseSeeder.java
+++ src/main/java/com/bhaumik18/finguard/config/DatabaseSeeder.java
@@ -7,39 +7,43 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.bhaumik18.finguard.transaction.dto.TransactionRequest;
 import com.bhaumik18.finguard.transaction.entity.Transaction;
 import com.bhaumik18.finguard.transaction.entity.TransactionStatus;
 import com.bhaumik18.finguard.transaction.entity.TransactionType;
+import com.bhaumik18.finguard.transaction.mapper.TransactionMapper;
 import com.bhaumik18.finguard.transaction.repository.TransactionRepository;
 
+import lombok.RequiredArgsConstructor;
+
+
 @Configuration
+@RequiredArgsConstructor
 public class DatabaseSeeder {
 	
+	private final TransactionRepository repository;
+	private final TransactionMapper mapper;
+	
 	@Bean
-	CommandLineRunner initDatabase(TransactionRepository repository) {
+	CommandLineRunner initDatabase() {
 		return args -> {
-			System.out.println("--- Bootstrapping FinGuard Sentinel ---");
 			
-			Transaction txn = Transaction.builder()
-                    .transactionReference("TXN-" + UUID.randomUUID().toString().substring(0, 8))
-                    .sourceAccountId("ACC-1001")
-                    .destinationAccountId("ACC-9999")
-                    .amount(new BigDecimal("1500.0000"))
-                    .currency("USD")
-                    .status(TransactionStatus.PENDING)
-                    .type(TransactionType.TRANSFER)
-                    .build();
 			
-			Transaction savedTxn = repository.save(txn);
-			System.out.println("Created: " + savedTxn.getTransactionReference() + " | Status: " + savedTxn.getStatus());
+			TransactionRequest request = new TransactionRequest(
+                    "ACC-1001",
+                    "ACC-9999",
+                    new BigDecimal("2500.00"),
+                    "USD",
+                    TransactionType.TRANSFER
+            );
 			
-			Thread.sleep(2000);
+			Transaction txn = mapper.toEntity(request);
 			
-			savedTxn.setStatus(TransactionStatus.COMPLETED);
-			repository.save(savedTxn);
-			System.out.println("Updated: " + savedTxn.getTransactionReference() + " | Status: " + savedTxn.getStatus());
-		
-			System.out.println("--- Bootstrapping Complete. Check H2 Console! ---");
+			txn.setTransactionReference("TXN-" + UUID.randomUUID().toString().substring(0, 8));
+			txn.setStatus(TransactionStatus.PENDING);
+			
+			repository.save(txn);
+			System.out.println("--- Milestone 2 Smoke Test Successful ---");
 		};
 	}
 }
