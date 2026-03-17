d210029a3873b534f40dd355708bb9526f04294f
Bhaumik <bhaumik182001@gmail.com>
2026-03-17 19:54:19 +0530

    feat(transactions): implement eventual consistency async state machine and fix CI/CD postgres pipeline

--- src/main/java/com/bhaumik18/finguard/transaction/listener/TransactionNotificationListener.java
+++ src/main/java/com/bhaumik18/finguard/transaction/listener/TransactionNotificationListener.java
@@ -1,6 +1,11 @@
 package com.bhaumik18.finguard.transaction.listener;
 
+import com.bhaumik18.finguard.transaction.entity.Transaction;
+import com.bhaumik18.finguard.transaction.entity.TransactionStatus;
 import com.bhaumik18.finguard.transaction.event.TransactionCompletedEvent;
+import com.bhaumik18.finguard.transaction.repository.TransactionRepository;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -9,7 +14,10 @@ import org.springframework.transaction.event.TransactionalEventListener; // NEW
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class TransactionNotificationListener {
+	
+	private final TransactionRepository transactionRepository;
 
     @Async("notificationExecutor")
     // This tells Spring: "Do NOT fire this event until the database commit is 100% successful!"
@@ -17,14 +25,20 @@ public class TransactionNotificationListener {
     public void handleTransactionCompleted(TransactionCompletedEvent event) {
         
         log.info("--- ASYNC WORKER STARTED ---");
-        log.info("Simulating connection to Email Server (AWS SES / SendGrid)...");
+        log.info("Simulating connection to 3rd Party Bank Processor...");
         
         try {
             Thread.sleep(3000); 
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
+        
+        Transaction transaction = transactionRepository.findByTransactionReference(event.transactionId())
+                .orElseThrow(() -> new RuntimeException("Transaction not found for Async Update"));
 
+        transaction.setStatus(TransactionStatus.COMPLETED);
+        transactionRepository.save(transaction);
+        
         log.info("EMAIL SENT: Successfully notified {} about transfer of ${} (Ref: {})", 
                  event.userEmail(), event.amount(), event.transactionId());
         log.info("--- ASYNC WORKER FINISHED ---");
--- src/main/java/com/bhaumik18/finguard/transaction/repository/TransactionRepository.java
+++ src/main/java/com/bhaumik18/finguard/transaction/repository/TransactionRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID>, JpaSpecificationExecutor<Transaction>{
 
+	Optional<Transaction> findByTransactionReference(String transactionReference);
+	
 }
